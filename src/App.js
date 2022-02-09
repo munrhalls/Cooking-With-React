b1ca31c8535af258b57b4383b6678b5089e7e2fa
John <jpilik@fream.pl>
2022-02-09 11:18:35 +0100

    Context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import React, { useState } from "react";
 import { CounterHooks } from "./components/CounterHooks";
 
+export const ThemeContext = React.createContext();
+
 function App() {
-  return <CounterHooks />;
+  const [theme, setTheme] = useState("purple");
+  return (
+    <ThemeContext.Provider value={{ backgroundColor: theme }}>
+      <CounterHooks />
+      <button
+        onClick={() =>
+          setTheme((prevTheme) => {
+            return prevTheme === "purple" ? "blue" : "purple";
+          })
+        }
+      >
+        Toggle prevTheme
+      </button>
+    </ThemeContext.Provider>
+  );
 }
 
 export default App;
--- src/components/CounterHooks.js
+++ src/components/CounterHooks.js
@@ -1,12 +1,19 @@
 import { useState } from "react";
+import { useContext } from "react";
+import { ThemeContext } from "../App";
 
 export const CounterHooks = () => {
   const [state, setState] = useState(0);
+  const style = useContext(ThemeContext);
   return (
     <>
-      <button onClick={() => setState(state - 1)}>-</button>
+      <button style={style} onClick={() => setState(state - 1)}>
+        -
+      </button>
       <span>{state}</span>
-      <button onClick={() => setState(state + 1)}>+</button>
+      <button style={style} onClick={() => setState(state + 1)}>
+        +
+      </button>
     </>
   );
 };
